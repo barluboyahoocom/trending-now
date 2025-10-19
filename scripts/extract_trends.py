9d69947e8d0bc013ef7e6e9154409759475f7146
barluboyahoocom <barlubo@yahoo.com>
2025-10-19 18:40:17 +0300

    Update extract_trends.py

--- scripts/extract_trends.py
+++ scripts/extract_trends.py
@@ -5,8 +5,8 @@ from deep_translator import GoogleTranslator
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 # GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
-GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
 # GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent"
+GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent"
 
 GEMINI_20_FLASH_LIMITS = {
     "MAX_RPM": 14,        # Requests per minute
